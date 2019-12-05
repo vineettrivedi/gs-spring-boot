80e03330aea0f2b70ff3a29d49e009073823933a
Vineet Trivedi <trivedi.v@husky.neu.edu>
2019-12-04 23:16:54 -0500

    empty test

--- complete/src/test/java/hello/HelloControllerTest.java
+++ complete/src/test/java/hello/HelloControllerTest.java
@@ -22,10 +22,10 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
-//     @Test
-//     public void getHello() throws Exception {
+    @Test
+    public void getHello() throws Exception {
 //         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 //                 .andExpect(status().isOk())
 //                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-//     }
+    }
 }
