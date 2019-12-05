e1a3121ab03f6b518c9347021be3ce74ac7ed04f
Vineet Trivedi <trivedi.v@husky.neu.edu>
2019-12-05 00:54:58 -0500

    greetings from spring boot

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Calvin and Hobbes!";
+        return "Greetings from Spring Boot!";
     }
     
 }
