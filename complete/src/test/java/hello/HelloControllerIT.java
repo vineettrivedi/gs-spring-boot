664f38fc77a7e0e4a2a366238fffb1afd0d93e8b
Vineet Trivedi <trivedi.v@husky.neu.edu>
2019-12-05 00:56:28 -0500

    uncommented hello controller it

--- complete/src/test/java/hello/HelloControllerIT.java
+++ complete/src/test/java/hello/HelloControllerIT.java
@@ -34,8 +34,8 @@ public class HelloControllerIT {
 
     @Test
     public void getHello() throws Exception {
-//         ResponseEntity<String> response = template.getForEntity(base.toString(),
-//                 String.class);
-//         assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
     }
 }
